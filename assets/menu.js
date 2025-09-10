6f46d524efc930a298f3b47296e416f025b3d7a8
Lukitas Luna <lukitaass2013@gmail.com>
2025-09-09 23:03:11 -0600

    Subiendo actualizacion de LEL Desarrollos Web

--- assets/menu.js
+++
@@ -1,5 +0,0 @@
-const btnmenu = document.querySelector(".icon-menu");
-const menu  = document.querySelector(".menu");
-btnmenu.addEventListener("click",()=>{
- menu.classList.toggle("mostrar");
-});
